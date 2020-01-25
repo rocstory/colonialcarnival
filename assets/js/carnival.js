79b94da88764418df7e53934614d45376ec28124
Malik Roc <maliktroc@outlook.com>
2020-01-25 13:52:57 -0500

    Add files via upload

--- assets/js/carnival.js
+++ assets/js/carnival.js
@@ -3,6 +3,14 @@
  * The JavaScript file for the carnival's home page.
  */
 
+function Item()
+{
+    this.name   = null; // make array 
+    this.amount = null;
+}
+
+var itemsArray = new Array();
+
 document.getElementById('WesternTrivia_img').addEventListener('click', ()=>{
     window.location = './wcsuTrivia.html';
 });
@@ -43,5 +51,58 @@ window.addEventListener('load', (event) =>{
         });
 
         document.getElementById('footer-name').innerHTML = 'Hi, ' + formattedName + '!';
+
+        // Get the carnival items and then populate the table.
+        GET_CARNIVAL_ITEMS().then(()=>
+        {
+            // the items array is populated now
+            // create the table.
+            createItemTable();
+        });
     });
 });
+
+
+/**
+ * 
+ * Load Carnival Items from the database
+ * 
+ */
+
+function GET_CARNIVAL_ITEMS()
+{
+    
+    return new Promise((resolve, reject)=>{
+        db.collection('carnivalitems').get().then((allItems) =>
+        {
+           
+            allItems.forEach((doc)=>{
+                var newItem = new Item;
+                newItem.name   = doc.data().name;
+                newItem.amount = doc.data().amount;
+                itemsArray.push(newItem);
+            });
+            resolve();
+        }).catch((error)=>
+        {
+            console.log('Error!', error);
+        });
+    })
+};
+
+function createItemTable()
+{
+    
+    var table = document.getElementById('itemTable');
+    for (var i = 0; i < itemsArray.length; i++)
+    {
+        var row = document.createElement("tr");
+        var nameCol = document.createElement('td');
+        var marbleCol = document.createElement('td');
+        nameCol.innerHTML = itemsArray[i].name;
+        marbleCol.innerHTML = itemsArray[i].amount;
+        row.appendChild(nameCol);
+        row.appendChild(marbleCol);
+        table.appendChild(row);
+    }
+};
